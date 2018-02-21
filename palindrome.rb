26d5320f4d3cde2b1a9fd6d1885db4259ffb75df
Ian Harwell <sirharwell@gmail.com>
2018-02-21 14:34:01 -0700

    permutations

--- palindrome.rb
+++ palindrome.rb
@@ -1,29 +1,39 @@
-def input
-  print  `clear`
-  puts "Go ahead and try and make a palindrome."
-  @guess = gets.strip.downcase.scan /\w/
-  @clone = @guess
-  @answer = @clone.reverse
-  output
+def opening
+  puts "What would you like to do today?"
+  puts "1) Test a palindrome"
+  puts "2) Test all combos"
+  puts ""
+  response = gets.to_i
+  case response
+  when 1
+    palindrome
+  when 2
+    combos
+  else
+    opening
+  end
 end
 
-def output
-  if @answer == @guess
+def palindrome
+  print  `clear`
+  puts "Go ahead and try and make a palindrome."
+  guess = gets.strip.downcase.scan /\w/
+  clone = guess
+  answer = clone.reverse
+  if answer == guess
     puts "Huzzah! You made a palindrome!"
   else
     puts "Boo! You Fail!"
   end
- tryagain
+ opening
 end
 
-def tryagain
-  puts "Do you want to try another one?"
-  response = gets.strip.downcase
-  if response == "yes" || response == "y"
-    input
-  else
-    exit
-  end
+def combos
+  print `clear`
+  puts "Type something to get all possible combos"
+  answer = gets.strip.downcase.scan /\w/
+  puts answer.permutation.map(&:join)
+  opening
 end
 
-input
+opening
